e79b36d4ed7e007a13d3ea88168a8949a7ed46b4
Raymond Somai <k00lrav@icloud.com>
2023-04-11 18:17:50 -0400

    corrected header names

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 function Header() {
   return (
     <div className ="header-container">
-        <div className="title">Fake Store</div>
-        <div className="logo"><img src="./src/assets/shoppingcart.svg" alt="" /></div>
+        <div className="logo-title">Fake Store</div>
+        <div className="shopping-cart"><img src="./src/assets/shoppingcart.svg" alt="shopping-cart" /></div>
         </div>
   )
 }
