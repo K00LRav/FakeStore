e0d8d90e9d8ee79f9eddc575d9fa3c6d2dd9d05d
Raymond Somai <k00lrav@icloud.com>
2023-04-11 18:21:17 -0400

    made changes to logo and shopping cart

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Header() {
   return (
     <div className ="header-container">
-        <div className="logo-title">Fake Store</div>
+        <div className="logo"><img src="./src/assets/logo.svg" alt="" /></div>
         <div className="shopping-cart"><img src="./src/assets/shoppingcart.svg" alt="shopping-cart" /></div>
         </div>
   )
