ed6edbf1353e83feebc8395e829aabccf0a25798
Raymond Somai <k00lrav@icloud.com>
2023-04-11 17:48:02 -0400

    added shopping cart

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 function Header() {
   return (
     <div className ="header-container">
         <div className="title">Fake Store</div>
+        <div className="logo"><img src="./src/assets/shoppingcart.svg" alt="" /></div>
         </div>
   )
 }
