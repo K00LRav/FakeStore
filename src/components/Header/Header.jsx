4bb54cf0eb43c43d475aea1a67cfd5d4f4a1740f
Raymond Somai <k00lrav@icloud.com>
2023-05-07 20:48:59 -0400

    made changes to the header Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,14 +10,14 @@ function Header() {
 
   return (
     <div className ="header-container">
-        <a href='/'><div className="logo"><img src="./src/assets/logo.svg" alt="" /></div></a>
+        <Link to='/'><div className="logo"><img src="./src/assets/logo.svg" alt="" /></div></Link>
         <div className="shopping-cart">
           <p>
             {
           cart.length 
           }
           </p>
-          <Link to='./CheckOut'><img src="./src/assets/shoppingcart.svg" alt="shopping-cart" /></Link>
+          <Link to='./Checkout'><img src="./src/assets/shoppingcart.svg" alt="shopping-cart" /></Link>
           </div>
         </div>
   )
